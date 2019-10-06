81526d71ac9947fa0339c422e7e7aae08061e75a
shreyrohatgi <rohatgishrey@gmail.com>
2019-10-07 01:05:37 +0530

    adding script

---
+++ script.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+import csv
+
+csv_links = open('links.csv', 'w')
+writer_link = csv.writer(csv_links)
+
+driver = webdriver.Chrome(executable_path='/home/shrey/rera_import/selenium_sample/chromedriver')
+driver.get('https://up-rera.in/agents')
+
+for i in range(2, 3262):
+    if ((i - 2)%10 == 0) and (i != 2):
+        python_button = driver.find_elements_by_xpath("//a[@data-dt-idx='9']")[0]
+        python_button.click()
+    x = str(i)
+    if i < 10:
+        x = '0' + str(i)
+
+    reg_id = driver.find_elements_by_id("ctl00_ContentPlaceHolder1_grdagents_ctl" + x + "_lbluser")[0].text.strip()
+
+    name = 'ctl00$ContentPlaceHolder1$grdagents$ctl' + x + '$imgbtndetail'
+    python_button = driver.find_elements_by_xpath("//input[@name='" + name + "' and @type='image']")[0]
+    python_button.click()
+    driver.switch_to_window(driver.window_handles[1])
+
+    # Write in csv
+    csv_row = [driver.current_url, reg_id]
+    writer_link.writerow(csv_row)
+
+    print(i)
+
+    driver.close()
+    driver.switch_to_window(driver.window_handles[0])
+
+csv_links.close()
+driver.close()
+
+
+
+
