64737a9f92fe9917d6ba9861c01d2f0e00d90510
shreyrohatgi <rohatgishrey@gmail.com>
2019-10-08 00:29:13 +0530

    changes in script file

--- script.py
+++ script.py
@@ -1,19 +1,29 @@
 from selenium import webdriver
 import csv
 
-csv_links = open('links.csv', 'w')
+csv_links = open('links.csv', 'a')
 writer_link = csv.writer(csv_links)
 
 driver = webdriver.Chrome(executable_path='/home/shrey/rera_import/selenium_sample/chromedriver')
 driver.get('https://up-rera.in/agents')
 
-for i in range(2, 3262):
-    if ((i - 2)%10 == 0) and (i != 2):
+for i in range(310, 3268):
+    count = (i-2)/10
+    while count:
         python_button = driver.find_elements_by_xpath("//a[@data-dt-idx='9']")[0]
         python_button.click()
+        count -= 1
+
+
+    # if ((i - 2)%10 == 0) and (i != 2):
+    #     python_button = driver.find_elements_by_xpath("//a[@data-dt-idx='9']")[0]
+    #     python_button.click()
     x = str(i)
     if i < 10:
         x = '0' + str(i)
+
+    
+    # for i in range(0, count):
 
     reg_id = driver.find_elements_by_id("ctl00_ContentPlaceHolder1_grdagents_ctl" + x + "_lbluser")[0].text.strip()
 
@@ -22,7 +32,7 @@ for i in range(2, 3262):
     python_button.click()
     driver.switch_to_window(driver.window_handles[1])
 
-    # Write in csv
+        # Write in csv
     csv_row = [driver.current_url, reg_id]
     writer_link.writerow(csv_row)
 
